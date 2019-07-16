e1d45da8f7630558b88ebc7cd297196099b7cee5
Touqeer Abbas <touqeerabbas49@gmail.com>
2019-07-16 05:07:38 +0500

    Initial Commit

---
+++ calculate/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  calculate
+//
+//  Created by Touqeer Abbas on 7/16/19.
+//  Copyright © 2019 Touqeer Abbas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
